471ce5ba0b6041c0543367aed882cb68ca8ae707
tomgao <376801941@qq.com>
2022-12-16 16:37:54 +0900

    完成交互式网站的开发,开发了Article的增删改查,都是存在内存,不涉及数据库

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.models.Article
 import com.example.models.articles
 import io.ktor.server.routing.*
 import io.ktor.http.*
@@ -8,6 +9,7 @@ import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
+import io.ktor.server.util.*
 
 fun Application.configureRouting() {
 
@@ -23,18 +25,47 @@ fun Application.configureRouting() {
             }
             get("new") {
                 // show a page with fields for creating a new article
+                call.respond(FreeMarkerContent("new.ftl", model = null))
             }
             post {
                 // save an article
+                val formParameters = call.receiveParameters()
+                val title = formParameters.getOrFail("title")
+                val body = formParameters.getOrFail("body")
+                val newEntry = Article.newEntry(title, body)
+                articles.add(newEntry)
+                call.respondRedirect("/articles/${newEntry.id}")
             }
             get("{id}") {
                 // show an article with a specific id
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(FreeMarkerContent("show.ftl", mapOf("article" to articles.find { it.id == id })))
+
             }
             get("{id}/edit") {
                 // show a page with fields for editing an article
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(FreeMarkerContent("edit.ftl", mapOf("article" to articles.find { it.id == id })))
             }
             post("{id}") {
                 // update or delete an article
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                val formParameters = call.receiveParameters()
+                when(formParameters.getOrFail("_action")) {
+                    "update" -> {
+                        val index = articles.indexOf(articles.find { it.id == id })
+                        val title = formParameters.getOrFail("title")
+                        val body = formParameters.getOrFail("body")
+                        articles[index].title = title
+                        articles[index].body = body
+                        call.respondRedirect("/articles/$id")
+                    }
+                    "delete" -> {
+                        articles.removeIf{ it.id == id}
+                        call.respondRedirect("/articles")
+                    }
+                }
+
             }
         }
 
